024cae4b8c6ece5020505630de9347eb2024a37e
Vaner Teixeira <vanerst@gmail.com>
2018-09-25 01:03:05 -0300

    no message

--- index.php
+++ index.php
@@ -1,3 +1,131 @@
+<!DOCTYPE html>
+<html lang="">
+
+  <head>
+  <!--  site para colocar a div responsiva https://www.w3schools.com/css/css_grid_item.asp -->
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Plyn!</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url()?>index/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="<?php echo base_url()?>index/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link href="<?php echo base_url()?>index/vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url()?>index/css/freelancer.min.css" rel="stylesheet">
+     <link href="<?php echo base_url()?>index/css/imagem.css" rel="stylesheet">
+
+    
+
+
+
+
+  </head>
+
+  <body id="page-top">
+
+
+  <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
+      <div class="container" overflow=" hidden">
+      <h1 class="text-uppercase mb-0">PLYN!</h1>
+        <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="<?php echo base_url()?>index.php/add">ADD</a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="<?php echo base_url()?>index.php/list">LIST</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+      <p style="padding-top: 150px; padding-bottom: 60px;">Lorem ipsum adipiscing taciti duis dictum et lacus habitasse tellus, aliquet maecenas proin eget cursus faucibus tortor tellus ligula nec, pretium nisi maecenas urna sed sollicitudin facilisis nam. proin mattis sapien aliquam eleifend ipsum facilisis aenean, vehicula mi hendrerit etiam vulputate malesuada ligula, curabitur id augue turpis vivamus justo. aptent elit sed nisl ultrices magna nam accumsan vivamus felis pharetra turpis, donec vulputate ultricies a suspendisse sem quam mi luctus at nisl gravida, quisque nulla pellentesque enim rutrum taciti scelerisque malesuada nulla habitasse. diam litora justo libero urna quisque auctor, odio suspendisse euismod nostra semper, tempor viverra luctus mi et. 
+
+  Cursus vitae egestas nisl libero ut aliquam aptent velit aenean ante, iaculis suscipit eros varius risus mattis ultrices porttitor vel felis morbi, non quisque libero quisque scelerisque vivamus a suscipit ut. erat mollis vulputate praesent sagittis vehicula turpis commodo cursus proin, vitae mauris sagittis tincidunt donec curae ac placerat est himenaeos, laoreet luctus quisque platea commodo aliquet erat senectus. curabitur posuere habitasse diam nisi platea phasellus suscipit iaculis proin, rhoncus volutpat auctor maecenas scelerisque ad praesent tellus ac laoreet, nulla per vel blandit molestie aenean consequat mollis. himenaeos sed sollicitudin venenatis potenti sagittis libero adipiscing varius vehicula urna, sagittis maecenas sit augue vel fringilla inceptos etiam laoreet primis mattis, habitasse urna porttitor aliquam curabitur accumsan magna commodo elementum. 
+
+   </p>
+
+    <!-- Footer -->
+    
+          
+          <div class="col-md-4 mb-5 mb-lg-0">
+            <h4 class="text-uppercase mb-4">Contato para web</h4>
+            <ul class="list-inline mb-0">
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-facebook"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-google-plus"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-twitter"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-linkedin"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-dribbble"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          
+        </div>
+      </div>
+    
+    </footer>
+
+    
+
+   
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url()?>home_page/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url()?>home_page/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url()?>home_page/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?php echo base_url()?>home_page/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="<?php echo base_url()?>home_page/js/jqBootstrapValidation.js"></script>
+    <script src="<?php echo base_url()?>home_page/js/contact_me.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="<?php echo base_url()?>home_page/js/freelancer.min.js"></script>
+
+  </body>
+
+</html>
 <?php
 /**
  * CodeIgniter
